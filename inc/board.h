16af09670bbe2df349c4679ff8f13ac672e36969
ne0ndrag0n <ne0ndrag0n@users.noreply.github.com>
2022-05-30 22:55:08 -0400

    Event loop

--- inc/board.h
+++ inc/board.h
@@ -1,15 +1,42 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#define TWIST_SWIRLS_X	15
-#define TWIST_SWIRLS_Y	15
+#include <genesis.h>
+
+#define TWIST_SWIRLS_X		15
+#define TWIST_SWIRLS_Y		15
+#define TWIST_MAX_EVENTS	64
 
 #define USED_BUTTONS	( BUTTON_LEFT | BUTTON_RIGHT | BUTTON_UP | BUTTON_DOWN )
 
+// Swirl.type
+#define NO_SWIRL		-1
+
+// PaletteAnimation.type
+#define NO_ANIMATION    0
+#define TO_SELECTED     1
+#define TO_UNSELECTED   -1
+
 typedef struct {
-	u8 type;
-	u8 selected;
+	s8 type;
+	s16 remaining_steps;		// To or from 0x0FFF as determined by type
+} PaletteAnimation;
+
+typedef struct {
+	s8 type;
+	s8 selected;
+	PaletteAnimation palette_animation;
 } Swirl;
+
+typedef struct Board {
+	Swirl board[ TWIST_SWIRLS_X * TWIST_SWIRLS_Y ];
+	u16 select_anim_palette[ 16 ];
+	s32 score;
+	s8 dirty;
+	Sprite* on_screen_cursor;
+} Board;
+
+typedef void (*BoardEvent)( Board*, void* );
 
 void twist_gameplay();
 
--- src/board.c
+++ src/board.c
@@ -4,37 +4,72 @@
 #include "resources.h"
 #include "swirls.h"
 
-static void twist_init_board( Swirl* board ) {
+static void twist_init_board( Board* game_state ) {
+	memcpy( game_state->select_anim_palette, twist_swirl1.palette->data, 32 );
+	SPR_setVRAMTileIndex( game_state->on_screen_cursor, TILE_USER_INDEX + 16 );
+
 	for( int y = 0; y < TWIST_SWIRLS_Y; y++ ) {
 		for( int x = 0; x < TWIST_SWIRLS_X; x++ ) {
-			board[ TWIST_SWIRLS_X * y + x ] = ( Swirl ) { .type = random() % 4, .selected = FALSE };
+			game_state->board[ TWIST_SWIRLS_X * y + x ] = ( Swirl ) {
+				.type = random() % 4,
+				.selected = FALSE,
+				( PaletteAnimation ) {
+					.type = NO_ANIMATION,
+					.remaining_steps = 0
+				}
+			};
 		}
 	}
 }
 
-static void twist_draw_board( Swirl* board, s32 score ) {
-	VDP_clearPlane( BG_A, TRUE );
-	VDP_clearPlane( BG_B, TRUE );
-	VDP_clearPlane( WINDOW, TRUE );
+static void twist_draw_board( Board* game_state ) {
+	if( game_state->dirty == TRUE ) {
+		game_state->dirty = FALSE;
 
-	VDP_drawText( "Twist", 33, 1 );
+		VDP_drawText( "Twist", 33, 1 );
 
-	VDP_drawText( "Score:", 31, 26 );
+		VDP_drawText( "Score:", 31, 26 );
 
-	char scoreString[ 11 ] = { 0 };
-	sprintf( scoreString, "%d", score );
-	VDP_drawText( scoreString, 31, 27 );
+		char scoreString[ 11 ] = { 0 };
+		sprintf( scoreString, "%d", game_state->score );
+		VDP_drawText( scoreString, 31, 27 );
 
-	for( int y = 0; y < TWIST_SWIRLS_Y; y++ ) {
-		for( int x = 0; x < TWIST_SWIRLS_X; x++ ) {
-			VDP_fillTileMapRectInc(
-				BG_B,
-				TILE_ATTR_FULL( PAL1, FALSE, FALSE, FALSE, TILE_USER_INDEX + ( board[ TWIST_SWIRLS_X * y + x ].type * 4 ) ),
-				x * 2,
-				y * 2,
-				2,
-				2
-			);
+		for( int y = 0; y < TWIST_SWIRLS_Y; y++ ) {
+			for( int x = 0; x < TWIST_SWIRLS_X; x++ ) {
+				int index = TWIST_SWIRLS_X * y + x;
+
+				if( game_state->board[ index ].type != NO_SWIRL ) {
+					VDP_fillTileMapRectInc(
+						BG_B,
+						TILE_ATTR_FULL(
+							PAL1,
+							FALSE,
+							FALSE,
+							FALSE,
+							TILE_USER_INDEX + ( game_state->board[ index ].type * 4 )
+						),
+						x * 2,
+						y * 2,
+						2,
+						2
+					);
+				} else {
+					VDP_fillTileMapRect(
+						BG_B,
+						TILE_ATTR_FULL(
+							game_state->board[ index ].palette_animation.type == NO_ANIMATION ? PAL1 : PAL2,
+							FALSE,
+							FALSE,
+							FALSE,
+							0
+						),
+						x * 2,
+						y * 2,
+						2,
+						2
+					);
+				}
+			}
 		}
 	}
 }
@@ -44,29 +79,36 @@ static void twist_load_swirl_gfx() {
 	VDP_loadTileSet( twist_swirl2.tileset, TILE_USER_INDEX + 4, CPU );
 	VDP_loadTileSet( twist_swirl3.tileset, TILE_USER_INDEX + 8, CPU );
 	VDP_loadTileSet( twist_swirl4.tileset, TILE_USER_INDEX + 12, CPU );
+
+	PAL_setColors( 16, twist_swirl1.palette->data, 16, CPU );
+	PAL_setColors( 32, twist_swirl1.palette->data, 16, CPU );
 }
 
 static u16 joyDump = 0;
 
 static void twist_board_handler( u16 joy, u16 changed, u16 state ) {
-	switch (joy ) {
+	switch ( joy ) {
 		case JOY_1: {
 			joyDump = state;
 		}
 	}
 }
 
-static void twist_update_animations( Sprite* onScreenCursor ) {
+static void twist_update_cursor( Board* game_state ) {
 	static Vect2D_s16 animDirection = ( Vect2D_s16 ){ .x = 0, .y = 0 };
 	static u16 remaining = 0;
 
 	if( remaining > 0 ) {
-		SPR_setPosition( onScreenCursor, onScreenCursor->x - 0x80 + ( animDirection.x * 4 ), onScreenCursor->y - 0x80 + ( animDirection.y * 4 ) );
+		SPR_setPosition(
+			game_state->on_screen_cursor,
+			game_state->on_screen_cursor->x - 0x80 + ( animDirection.x * 4 ),
+			game_state->on_screen_cursor->y - 0x80 + ( animDirection.y * 4 )
+		);
 
 		remaining--;
 	} else {
 		// Check joyDump for the need to play a new animation
-		Vect2D_s16 currentPosition = ( Vect2D_s16 ){ .x = onScreenCursor->x - 0x80, .y = onScreenCursor->y - 0x80 };
+		Vect2D_s16 currentPosition = ( Vect2D_s16 ){ .x = game_state->on_screen_cursor->x - 0x80, .y = game_state->on_screen_cursor->y - 0x80 };
 		if( joyDump & BUTTON_RIGHT && currentPosition.x < 224 ) {
 			animDirection = ( Vect2D_s16 ){ .x = 1, .y = 0 };
 			remaining = 4;
@@ -83,29 +125,51 @@ static void twist_update_animations( Sprite* onScreenCursor ) {
 	}
 }
 
+static void twist_execute_events( Board* game_state, void* events_pointer ) {
+	BoardEvent* events = ( BoardEvent* ) events_pointer;
+
+	for( int i = 0; i < TWIST_MAX_EVENTS; i++ ) {
+		if( events[ i ] ) {
+			events[ i ]( game_state, events_pointer );
+			events[ i ] = NULL;
+		}
+	}
+}
+
 void twist_gameplay() {
+	VDP_clearPlane( BG_A, TRUE );
+	VDP_clearPlane( BG_B, TRUE );
+	VDP_clearPlane( WINDOW, TRUE );
+
 	JOY_setEventHandler( twist_board_handler );
 
-	Swirl board[ TWIST_SWIRLS_X * TWIST_SWIRLS_Y ] = { 0 };
-	s32 score = 0;
-
-	twist_load_swirl_gfx();
-	twist_init_board( board );
-	twist_draw_board( board, score );
-
 	u16** frames = SPR_loadAllFrames( &cursor, TILE_USER_INDEX + 16, NULL );
-	Sprite* onScreenCursor = SPR_addSpriteExSafe(
-		&cursor,
-		0,
-		0,
-		TILE_ATTR( PAL1, TRUE, FALSE, FALSE ),
-		NULL,
-		SPR_FLAG_AUTO_VISIBILITY | SPR_FLAG_AUTO_SPRITE_ALLOC | SPR_FLAG_AUTO_TILE_UPLOAD
-	);
-	SPR_setVRAMTileIndex( onScreenCursor, TILE_USER_INDEX + 16 );
+	twist_load_swirl_gfx();
+
+	// Game state
+	Board game_state = ( Board ) {
+		.board = { 0 },
+		.select_anim_palette = { 0 },
+		.score = 0,
+		.dirty = TRUE,
+		.on_screen_cursor = SPR_addSpriteExSafe(
+			&cursor,
+			0,
+			0,
+			TILE_ATTR( PAL1, TRUE, FALSE, FALSE ),
+			NULL,
+			SPR_FLAG_AUTO_VISIBILITY | SPR_FLAG_AUTO_SPRITE_ALLOC | SPR_FLAG_AUTO_TILE_UPLOAD
+		)
+	};
+
+	twist_init_board( &game_state );
+
+	BoardEvent events[ TWIST_MAX_EVENTS ] = { NULL };
 
 	while( TRUE ) {
-		twist_update_animations( onScreenCursor );
+		twist_update_cursor( &game_state );
+		twist_draw_board( &game_state );
+		twist_execute_events( &game_state, events );
 
 		SPR_update();
 		SYS_doVBlankProcess();
